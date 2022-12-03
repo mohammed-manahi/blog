c2b3c8ba408b0da48eb038cb0f36064d3c7de0ee
Mohammed Manahi <mohammedsaadmanahi@hotmail.com>
2022-12-03 05:20:24 +0300

    Add post model to django admin site

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from main.models import Post
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    # Register post model to admin site
+    list_display = ["title", "slug", "author", "publish", "status"]
+    list_filter = ["status", "created", "publish", "author"]
+    search_fields = ["title", "body"]
+    prepopulated_fields = {"slug": ("title",)}
+    date_hierarchy = "publish"
+    ordering = ["publish", "status"]
+    autocomplete_fields = ["author"]
