d211c17013dc1927508a188fbce5b8d14b93d8a5
Mohammed Manahi <mohammedsaadmanahi@hotmail.com>
2022-12-04 22:08:58 +0300

    Implement class-based view with pagination for post list

--- main/urls.py
+++ main/urls.py
@@ -5,7 +5,8 @@ app_name = "main"
 
 urlpatterns = [
     # Define url patterns for post list and post detail views
-    path("", views.post_list, name="post_list"),
+    # path("", views.post_list, name="post_list"),
+    path("", views.PostListView.as_view(), name="post_list"),
     # Make urls seo friendly by using date and slug combination in the post detail url
     path("<int:day>/<int:month>/<int:year>/<slug:post>/", views.post_detail, name="post_detail"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,22 +1,27 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 from main.models import Post
-from django.core.paginator import Paginator, EmptyPage
 
 
-def post_list(request):
-    # Create post list to fetch all published posts
-    posts = Post.published.all()
-    # Use paginator to split content on pages
-    paginator = Paginator(posts, 3)
-    page_number = request.GET.get("page", 1)
-    try:
-        post_list = paginator.page(page_number)
-    except EmptyPage:
-        post_list = paginator.page(page_number.num_pages)
-    template = "main/post_list.html"
-    # Use paginated post list in the context
-    context = {"post_list": post_list}
-    return render(request, template, context)
+# def post_list(request):
+#     # Create post list to fetch all published posts
+#     posts = Post.published.all()
+#     # Use paginator to split content on pages
+#     paginator = Paginator(posts, 3)
+#     page_number = request.GET.get("page", 1)
+#     try:
+#         post_list = paginator.page(page_number)
+#     except PageNotAnInteger:
+#         # Handle page is not an integer exception
+#         post_list = paginator.page(1)
+#     except EmptyPage:
+#         # Handle empty page exception
+#         post_list = paginator.page(page_number.num_pages)
+#     template = "main/post_list.html"
+#     # Use paginated post list in the context
+#     context = {"post_list": post_list}
+#     return render(request, template, context)
 
 
 def post_detail(request, day, month, year, post):
@@ -26,3 +31,12 @@ def post_detail(request, day, month, year, post):
     template = "main/post_detail.html"
     context = {"post": post}
     return render(request, template, context)
+
+
+class PostListView(ListView):
+    # Create class-based view for post list
+    queryset = Post.published.all()
+    paginate_by = 3
+    # Use context object name, if not set default is object list
+    context_object_name = "post_list"
+    template_name = "main/post_list.html"
