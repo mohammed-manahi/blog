b2ad726768d63a0ad7dd4c448cd00f65ef148727
Mohammed Manahi <mohammedsaadmanahi@hotmail.com>
2022-12-08 17:25:54 +0300

    Implement custom inclusion tag to show the latest posts using template tags

--- main/templatetags/main_tags.py
+++ main/templatetags/main_tags.py
@@ -9,3 +9,10 @@ register = template.Library()
 def total_posts():
     # Register a new custom simple tag that returns a string of posts count for the template
     return Post.published.count()
+
+
+@register.inclusion_tag("main/latest_posts.html")
+def latest_posts(count=5):
+    # Register a new custom inclusion tag that renders a dictionary of values for the template of the latest posts
+    latest_posts = Post.published.order_by("-publish")[:count]
+    return {"latest_posts": latest_posts}
