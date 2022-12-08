6fb4ec677d140b98fddb0296fec0a5c62390e727
Mohammed Manahi <mohammedsaadmanahi@hotmail.com>
2022-12-08 17:44:42 +0300

    Implement custom simple tag to show most commented posts using template tags

--- main/templatetags/main_tags.py
+++ main/templatetags/main_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.db.models import Count
 from main.models import Post
 
 # Register new custom tag in template library
@@ -6,13 +7,19 @@ register = template.Library()
 
 
 @register.simple_tag
-def total_posts():
+def get_total_posts():
     # Register a new custom simple tag that returns a string of posts count for the template
     return Post.published.count()
 
 
+@register.simple_tag
+def get_most_commented_posts(count=3):
+    # Register a new custom simple tag that returns the most commented posts
+    return Post.published.annotate(total_comments=Count("comments")).order_by("-total_comments")[:count]
+
+
 @register.inclusion_tag("main/latest_posts.html")
-def latest_posts(count=5):
+def get_latest_posts(count=5):
     # Register a new custom inclusion tag that renders a dictionary of values for the template of the latest posts
     latest_posts = Post.published.order_by("-publish")[:count]
     return {"latest_posts": latest_posts}
